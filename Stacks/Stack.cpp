621917bbbf4a3dc29ea24a3369f53d395563733c
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-05-08 19:23:46 +0530

    completed bit manipulation

---
+++ Stacks/Stack.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+    int main(){
+    stack<int> s;
+    s.push(2);
+    s.push(3);
+    s.pop();
+    cout<<s.top()<<endl;
+    cout<<s.size()<<endl;
+    return 0;
+};
