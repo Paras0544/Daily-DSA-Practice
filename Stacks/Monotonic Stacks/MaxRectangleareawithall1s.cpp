c47a9a04c7279359b2ba9c436cd8bb2a021347ed
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-05-23 16:49:38 +0530

    largestrectangle

---
+++ Stacks/Monotonic Stacks/MaxRectangleareawithall1s.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+
+    int largestRectangle(vector<int>& heights) {
+
+        int n = heights.size();
+        int maxArea = 0;
+        stack<int> st;
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() && heights[i] < heights[st.top()]) {
+
+                int height = heights[st.top()];
+                st.pop();
+
+                int width = st.empty() ? i : i - st.top() - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        // IMPORTANT PART
+        while(!st.empty()) {
+
+            int height = heights[st.top()];
+            st.pop();
+
+            int width = st.empty() ? n : n - st.top() - 1;
+
+            maxArea = max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+        if(matrix.empty()) return 0;
+
+        int m = matrix[0].size();
+
+        vector<int> height(m, 0);
+
+        int maxArea = 0;
+
+        for(auto& row : matrix) {
+
+            for(int i = 0; i < m; i++) {
+
+                if(row[i] == '1')
+                    height[i]++;
+                else
+                    height[i] = 0;
+            }
+
+            maxArea = max(maxArea, largestRectangle(height));
+        }
+
+        return maxArea;
+    }
+};
+
+int main() {
+
+    vector<vector<char>> matrix = {
+        {'1','0','1','0','0'},
+        {'1','0','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','0','0','1','0'}
+    };
+
+    Solution obj;
+
+    cout << obj.maximalRectangle(matrix) << endl;
+
+    return 0;
+}
