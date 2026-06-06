98f1b5d5e14a0cf77d6ccf7fd90835d9ebb0c3e2
Paras Miglani <129984239+Paras0544@users.noreply.github.com>
2026-06-06 16:30:48 +0530

    started heaps

---
+++ Heap/isMinHeap.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+
+    bool isMinHeap(vector<int>& nums){
+        int n = nums.size();
+
+        for(int i = 0; i <= (n/2) - 1; i++){
+            int left = 2*i+1;
+            int right = 2*i+2;
+
+            if(left < n && nums[i] > nums[left]){
+                return false;
+            }
+            if(right < n && nums[i] > nums[right]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {10, 20, 30, 15, 23};
+
+    // Output result
+    cout << (obj.isMinHeap(nums) ? "true" : "false") << endl;
+    return 0;
+}
